41e8889da26c887800d9b297389ba5d36e59d10d
Boncroix <boncroix@gmail.com>
2023-09-11 22:21:13 +0200

    build: Agregamos archivos que vamos a utilizar en el proyecto

--- arkanoid/game.py
+++ arkanoid/game.py
@@ -1,25 +1,38 @@
-import pygame
+import pygame as pg
 from . import ANCHO, ALTO, COLOR_FONDO
+from .escenas import MejoresJugadores, Partida, Portada
+# from arkanoid.escenas import MejoresJugadores, Partida, Portada
 
 
 class Arkanoid():
     def __init__(self):
-        pygame.init()
-        pygame.display.set_caption('Arkanoid')
-        self.pantalla = pygame.display.set_mode((ANCHO, ALTO))
+        pg.init()
+        pg.display.set_caption('Arkanoid')
+        self.pantalla = pg.display.set_mode((ANCHO, ALTO))
+
+        portada = Portada(self.pantalla)
+        partida = Partida(self.pantalla)
+        records = MejoresJugadores(self.pantalla)
+
+        self.escenas = [
+            portada,
+            partida,
+            records
+        ]
+
+        '''
+        # Escrito de forma abreviada, creamos una lista con los objetos
+        self.escenas = [
+            Portada(self.pantalla),
+            Partida(self.pantalla),
+            MejoresJugadores(self.pantalla)
+        ]   '''
 
     def jugar(self):
-        salir = False
-        while not salir:
-            for evento in pygame.event.get():
-                if evento.type == pygame.QUIT or (evento.type == pygame.KEYUP and evento.key == pygame.K_ESCAPE):
-                    salir = True
-            # 2. Calcular estado de elementos y pintarlos
-            self.pantalla.fill(COLOR_FONDO)
-            # 3. ostrar los cambios (pintados) y controlar el reloj
-            pygame.display.flip()
+        for escena in self.escenas:
+            escena.bucle_principal()
 
-        pygame.quit()
+        pg.quit()
 
 
 if __name__ == '__main__':
