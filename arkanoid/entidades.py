9db6f4d06f4b2c34b8c38136da7458541615fb0c
Boncroix <boncroix@gmail.com>
2023-09-12 23:19:26 +0200

    feat: movemos la pala dentro de los límites

--- arkanoid/entidades.py
+++ arkanoid/entidades.py
@@ -9,6 +9,10 @@ from .import ALTO, ANCHO
 
 
 class Raqueta(pg.sprite.Sprite):
+
+    margen = 25
+    velocidad = 15
+
     def __init__(self):
         super().__init__()
 
@@ -21,8 +25,8 @@ class Raqueta(pg.sprite.Sprite):
         self.contador = 0
         self.image = self.imagenes[self.contador]
 
-        margen = 25
-        self.rect = self.image.get_rect(midbottom=(ANCHO/2, ALTO - margen))
+        self.rect = self.image.get_rect(
+            midbottom=(ANCHO / 2, ALTO - self.margen))
 
     def update(self):
         # 00 -> 01 -> 02 -> 00 -> 01 -> 02
@@ -30,3 +34,15 @@ class Raqueta(pg.sprite.Sprite):
         if self.contador > 2:
             self.contador = 0
         self.image = self.imagenes[self.contador]
+
+        estado_teclas = pg.key.get_pressed()
+        if estado_teclas[pg.K_LEFT]:
+            if self.rect.x <= 0:
+                self.rect.x = 0
+            else:
+                self.rect.x -= self.velocidad
+        if estado_teclas[pg.K_RIGHT]:
+            if self.rect.x >= ANCHO - self.rect.width:
+                self.rect.x = ANCHO - self.rect.width
+            else:
+                self.rect.x += self.velocidad
