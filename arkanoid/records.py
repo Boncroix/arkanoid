fbe878df92eee2ab178e449be93db7fdc675eee0
Boncroix <boncroix@gmail.com>
2023-09-19 23:20:05 +0200

    feat: crear clase records

--- arkanoid/records.py
+++ arkanoid/records.py
@@ -1,3 +1,4 @@
+import csv
 import os
 
 MAX_RECORDS = 10
@@ -7,26 +8,64 @@ class Records:
 
     # __file__ hace referencia al archivo actual (records.py)
     filename = 'records.csv'
-    base_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    path = os.path.join(base_dir, 'data', filename)
+    file_dir = os.path.dirname(os.path.realpath(__file__))
 
     def __init__(self):
-        pass
+        self.game_records = []
+        self.data_path = os.path.join(
+            os.path.dirname(self.file_dir),
+            'data'
+        )
+        self.file_path = os.path.join(
+            self.data_path, self.filename
+        )
+        self.check_records_file()
+        self.cargar()
 
     def check_records_file(self):
-        pass
+        if not os.path.isdir(self.data_path):
+            os.makedirs(self.data_path)
+            print('No había directorio para datos, pero lo he creado!!!')
+        if not os.path.exists(self.file_path):
+            self.reset()
 
     def insertar_record(self, nombre, puntuacion):
-        pass
+        """
+        1. recorrer todo el archivo y ver donde insertarlo
+        2. escribir el archivo
+        """
+        self.game_records.append([nombre, puntuacion])
+        self.game_records.sort(key=lambda item: item[1], reverse=True)
+        self.game_records = self.game_records[:MAX_RECORDS]
+        self.guardar()
 
     def puntuacion_menor(self):
-        pass
+        return self.game_records[-1][1]
 
     def guardar(self):
-        pass
+        # lector = open(self.file_path, mode='w')
+        # lector.close()
+        with open(self.file_path, mode='w') as records_file:
+            writer = csv.writer(records_file)
+            writer.writerow(('Nombre', 'Puntos'))
+            writer.writerows(self.game_records)
 
     def cargar(self):
-        pass
+        with open(self.file_path, mode='r') as records_file:
+            reader = csv.reader(records_file)
+            contador = 0
+            self.game_records = []
+            for linea in reader:
+                contador += 1
+                if contador == 1:
+                    continue
+                self.game_records.append([linea[0], int(linea[1])])
 
     def reset(self):
-        pass
+        """
+        Crea el archivo de records VACÍO
+        """
+        self.game_records = []
+        for cont in range(MAX_RECORDS):
+            self.game_records.append(['-----', 0])
+        self.guardar()
